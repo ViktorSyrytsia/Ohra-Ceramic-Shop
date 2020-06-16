36c12cd4b207d9e9e7769c2b7d105437acb3abfd
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-06-17 00:07:05 +0300

    add loader

--- src/components/loadingIndicator/LoadingIndicator.jsx
+++ src/components/loadingIndicator/LoadingIndicator.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
-import { Message, Icon } from 'semantic-ui-react'
+import { Loader } from 'semantic-ui-react'
 
-const MessageExampleIcon = () => (
-        <Message icon>
-                <Icon name='circle notched' loading />
-                <Message.Content>
-                        <Message.Header>Just one second</Message.Header>
-      We are fetching that content for you.
-    </Message.Content>
-        </Message>
-)
+const LoadingIndicator = () => <Loader size='massive' active inline='centered' />
 
-export default MessageExampleIcon
+export default LoadingIndicator
