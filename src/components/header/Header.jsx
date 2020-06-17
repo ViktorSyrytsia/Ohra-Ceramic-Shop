997a1021c230e2db6e479aed555b522dd6663c36
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-06-17 19:43:19 +0300

    cart icons fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -33,9 +33,9 @@ const Header = ({ totalPrice, totalItems, items, removeFromCart }) => {
                                         <Popup
                                                 content='Add users to your feed'
                                                 trigger={
-                                                        <div className='trigger-container'>
-                                                                <span className="cart-icon"><Icon name='shopping cart' size='big' /></span>
-                                                                <span className="total-circ">{totalItems}</span>
+                                                        <div className='cart-icons'>
+                                                                <i aria-hidden="true" class="cart bordered icon"></i>
+                                                                <i aria-hidden="true" class="bordered icon">{totalItems}</i>
                                                         </div>
 
                                                 }
@@ -48,32 +48,7 @@ const Header = ({ totalPrice, totalItems, items, removeFromCart }) => {
 
                         </div>
                 </div >
-                // <Menu size='massive' color={color} inverted widths={4}>
-                //         <Menu.Item
-                //                 name='O H R A'
-                //                 onClick={onColorChange}
-                //         />
-                //         <Menu.Item
-                //                 name='home'
-                //                 active={activeItem === 'home'}
-                //                 onClick={handleItemClick}
-                //         />
-                //         <Menu.Item
-                //                 name='about'
-                //                 active={activeItem === 'about'}
-                //                 onClick={handleItemClick}
-                //         />
-                //         <Popup
-                //                 content='Add users to your feed'
-                //                 trigger={<Menu.Item
-                //                 ><span className="item">{<Icon name='shopping cart' size='large' />} Total:{totalPrice} Items:{totalItems}</span> </Menu.Item>}
-                //                 content={<CartPopup items={items} removeFromCart={removeFromCart} />}
-                //                 on='click'
-                //                 hideOnScroll
-                //         >
 
-                //         </Popup>
-                // </Menu>
         )
 }
 
