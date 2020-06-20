1aa099226b9e4e2a8e3a414723e1fc943dc3c74e
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-06-20 15:34:10 +0300

    remove containers

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-import { Container } from 'semantic-ui-react';
+
 
 import './AboutUs.scss';
 
 const AboutUs = () => {
         return (
-                <Container>
-                        <div className='aboutUs'>
-                                <h2 className='aboutUs-title'>About Us</h2>
-                                <img className='aboutUs-img' src='https://i.imgur.com/44QGs4O.png' alt='about us'></img>
-                                <div className='aboutUs-text'>Я роблю весь посуд, який ви можете тут побачити.
-                                Фотографую і веду профіль.
-                                Говорю з вами діректі і приймаю замовлення.
-                                Роблю ескізи.
-                                Відправляю посилочки.
+
+                <div className='aboutUs'>
+                        <h2 className='aboutUs-title'>About Us</h2>
+                        <img className='aboutUs-img' src='https://i.imgur.com/44QGs4O.png' alt='about us'></img>
+                        <div className='aboutUs-text'>Я роблю весь посуд, який ви можете тут побачити.
+                        Фотографую і веду профіль.
+                        Говорю з вами діректі і приймаю замовлення.
+                        Роблю ескізи.
+                        Відправляю посилочки.
 З недавніх пір проводжу майстер-класи. Мені подобається знайомити вас з глиною і показувати, що робити посуд можна не гончарством єдиним.</div>
-                        </div>
-                </Container>
+                </div>
+
 
         )
 }
